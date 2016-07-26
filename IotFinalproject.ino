161aaea315464d7414f63d37aa82d8d4808d2076
nivedhr <nivedhnvd@gmail.com>
2016-07-26 13:52:54 +0530

    This is the final code
    This is code is made to demonstrate my project on power plant control

---
+++ IotFinalproject.ino
@@ -0,0 +1,231 @@
+/*
+  WriteVoltage
+  
+  Reads an analog voltage from pin 0, and writes it to a channel on ThingSpeak every 20 seconds.
+  
+  ThingSpeak ( https://www.thingspeak.com ) is a free IoT service for prototyping
+  systems that collect, analyze, and react to their environments.
+  
+  Copyright 2015, The MathWorks, Inc.
+  
+  Documentation for the ThingSpeak Communication Library for Arduino is in the extras/documentation folder where the library was installed.
+  See the accompaning licence file for licensing information.
+*/
+
+#ifdef SPARK
+  #include "ThingSpeak/ThingSpeak.h"
+#else
+  #include "ThingSpeak.h"
+#endif
+
+/// ***********************************************************************************************************
+// This example selects the correct library to use based on the board selected under the Tools menu in the IDE.
+// Yun, Wired Ethernet shield, wi-fi shield, esp8266, and Spark are all supported.
+// With Uno and Mega, the default is that you're using a wired ethernet shield (http://www.arduino.cc/en/Main/ArduinoEthernetShield)
+// If you're using a wi-fi shield (http://www.arduino.cc/en/Main/ArduinoWiFiShield), uncomment the line below
+// ***********************************************************************************************************
+//#define USE_WIFI_SHIELD
+#ifdef ARDUINO_ARCH_AVR
+
+  #ifdef ARDUINO_AVR_YUN
+    #include "YunClient.h"
+    YunClient client;
+  #else
+
+    #ifdef USE_WIFI_SHIELD
+      #include <SPI.h>
+      // ESP8266 USERS -- YOU MUST COMMENT OUT THE LINE BELOW.  There's a bug in the Arduino IDE that causes it to not respect #ifdef when it comes to #includes
+      // If you get "multiple definition of `WiFi'" -- comment out the line below.
+      #include <WiFi.h>
+      char ssid[] = "<YOURNETWORK>";          //  your network SSID (name) 
+      char pass[] = "<YOURPASSWORD>";   // your network password
+      int status = WL_IDLE_STATUS;
+      WiFiClient  client;
+    #else
+      // Use wired ethernet shield
+      #include <SPI.h>
+      #include <Ethernet.h>
+      byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+      
+
+      EthernetClient client;
+    #endif
+  #endif
+  // On Arduino:  0 - 1023 maps to 0 - 5 volts
+  #define VOLTAGE_MAX 5.0
+  #define VOLTAGE_MAXCOUNTS 1023.0
+#endif
+
+#ifdef ARDUINO_ARCH_ESP8266
+  #include <ESP8266WiFi.h>
+  char ssid[] = "<YOURNETWORK>";          //  your network SSID (name) 
+  char pass[] = "<YOURPASSWORD>";   // your network password
+  int status = WL_IDLE_STATUS;
+  WiFiClient  client;
+  // On ESP8266:  0 - 1023 maps to 0 - 1 volts
+  #define VOLTAGE_MAX 1.0
+  #define VOLTAGE_MAXCOUNTS 1023.0
+#endif
+
+#ifdef SPARK
+    TCPClient client;
+    // On Particle: 0 - 4095 maps to 0 - 3.3 volts
+    #define VOLTAGE_MAX 3.3
+    #define VOLTAGE_MAXCOUNTS 4095.0
+#endif
+
+/*
+  *****************************************************************************************
+  **** Visit https://www.thingspeak.com to sign up for a free account and create
+  **** a channel.  The video tutorial http://community.thingspeak.com/tutorials/thingspeak-channels/ 
+  **** has more information. You need to change this to your channel, and your write API key
+  **** IF YOU SHARE YOUR CODE WITH OTHERS, MAKE SURE YOU REMOVE YOUR WRITE API KEY!!
+  *****************************************************************************************/
+unsigned long myChannelNumber = 126887;
+const char * myWriteAPIKey = "T6K1SPLBZ1RNL3J7";
+int pushButton=2;
+
+IPAddress ip(10, 4, 43, 123); // IP address, may need to change depending on network
+     EthernetServer server(80);  // create a server at port 80
+
+String HTTP_req;          // stores the HTTP request
+boolean LED_status = 0;   // state of LED, off by default
+void setup() {
+  #if defined(ARDUINO_ARCH_AVR) || defined(ARDUINO_ARCH_ESP8266)
+    #ifdef ARDUINO_AVR_YUN
+      Bridge.begin();
+    #else
+      #if defined(USE_WIFI_SHIELD) || defined(ARDUINO_ARCH_ESP8266)
+        WiFi.begin(ssid, pass);
+      #else
+        Ethernet.begin(mac);
+      #endif
+    #endif
+  #endif
+  
+   Ethernet.begin(mac, ip);  // initialize Ethernet device
+    server.begin();           // start to listen for clients
+    Serial.begin(9600);       // for diagnostics
+    Serial.println(Ethernet.localIP());
+  Serial.println("connecting...");
+  pinMode(8, OUTPUT); 
+  ThingSpeak.begin(client);
+    Serial.begin(9600);
+    pinMode(pushButton, INPUT);
+    
+          // 
+}
+
+void loop() {
+ 
+  // read the input on analog pin 0:
+  
+  int sensorValue = analogRead(A0);
+  int buttonState = digitalRead(pushButton);
+  // Convert the analog reading 
+  // On Arduino:  0 - 1023 maps to 0 - 5 volts
+  // On ESP8266:  0 - 1023 maps to 0 - 1 volts
+  // On Particle: 0 - 4095 maps to 0 - 3.3 volts
+  float voltage = sensorValue * (VOLTAGE_MAX / VOLTAGE_MAXCOUNTS);
+  Serial.println("\nPrinting Voltage" );
+   Serial.println(voltage);
+   Serial.println("\nElectricityDemand");
+   if(buttonState==1){
+   //Serial.println(buttonState);
+   Serial.println("NO");
+   }
+   else{
+     //Serial.println(buttonState);
+   Serial.println("YES");
+   }
+  
+  
+
+  // Write to ThingSpeak. There are up to 8 fields in a channel, allowing you to store up to 8 different
+  // pieces of information in a channel.  Here, we write to field 1.
+  ThingSpeak.writeField(myChannelNumber, 1, voltage, myWriteAPIKey);
+ThingSpeak.writeField(myChannelNumber, 2, buttonState, myWriteAPIKey);
+  delay(2000); // ThingSpeak will only accept updates every 15 seconds.
+
+  EthernetClient client = server.available();  // try to get client
+
+    if (client) {  // got client?
+        boolean currentLineIsBlank = true;
+        while (client.connected()) {
+            if (client.available()) {   // client data available to read
+                char c = client.read(); // read 1 byte (character) from client
+                HTTP_req += c;  // save the HTTP request 1 char at a time
+                // last line of client request is blank and ends with \n
+                // respond to client only after last line received
+                if (c == '\n' && currentLineIsBlank) {
+                    // send a standard http response header
+                    client.println("HTTP/1.1 200 OK");
+                    client.println("Content-Type: text/html");
+                    client.println("Connection: close");
+                    client.println();
+                    // send web page
+                    client.println("<!DOCTYPE html>");
+                    client.println("<html>");
+                    client.println("<head>");
+                    client.println("<title>POWERPLANT CONTROL</title>");
+                    client.println("</head>");
+                    client.println("<body>");
+                    client.println("<h1>POWER PLANT</h1>");
+                    client.println("<p>Click to SHUTDOWN POWER PLANT</p>");
+                    client.println("<form method=\"get\">");
+                    ProcessCheckbox(client);
+                    client.println("</form>");
+                    client.println("</body>");
+                    client.println("</html>");
+                    Serial.print(HTTP_req);
+                    HTTP_req = "";    // finished with request, empty string
+                    break;
+                }
+                // every line of text received from the client ends with \r\n
+                if (c == '\n') {
+                    // last character on line of received text
+                    // starting new line with next character read
+                    currentLineIsBlank = true;
+                } 
+                else if (c != '\r') {
+                    // a text character was received from client
+                    currentLineIsBlank = false;
+                }
+            } // end if (client.available())
+        } // end while (client.connected())
+        delay(1);      // give the web browser time to receive the data
+        client.stop(); // close the connection
+    } // end if (client)
+  
+   
+    
+}
+
+// switch LED and send back HTML for LED checkbox
+void ProcessCheckbox(EthernetClient cl)
+{
+    if (HTTP_req.indexOf("PLANT=2") > -1) {  // see if checkbox was clicked
+        // the checkbox was clicked, toggle the LED
+        if (LED_status) {
+            LED_status = 0;
+        }
+        else {
+            LED_status = 1;
+        }
+    }
+    
+    if (LED_status) {    // switch LED on
+        digitalWrite(8, HIGH);
+        // checkbox is checked
+        cl.println("<input type=\"checkbox\" name=\"PLANT\" value=\"2\" \
+        onclick=\"submit();\" checked>PLANT");
+    }
+    else {              // switch LED off
+        digitalWrite(8, LOW);
+        // checkbox is unchecked
+        cl.println("<input type=\"checkbox\" name=\"PLANT\" value=\"2\" \
+        onclick=\"submit();\">PLANT");
+    }
+}
+
+
